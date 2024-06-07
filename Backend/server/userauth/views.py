b2ccaff601cd18c3057ee3a90259480722e73b21
graham218 <grahambill011@gmail.com>
2024-06-08 00:25:01 +0300

    Created views on 'server/views.py'

--- Backend/server/userauth/views.py
+++ Backend/server/userauth/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer, MyTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+User = get_user_model()
+
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
