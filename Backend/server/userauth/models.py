ea8557a2dc6d2a81a1f05e912803e477b2e0fb08
graham218 <grahambill011@gmail.com>
2024-06-08 00:22:26 +0300

    created Custom User Model

--- Backend/server/userauth/models.py
+++ Backend/server/userauth/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email_verified = models.BooleanField(default=False)
+    role = models.CharField(max_length=255, choices=[('admin', 'Admin'), ('user', 'User')], default='user')
+
+    def __str__(self):
+        return self.username
